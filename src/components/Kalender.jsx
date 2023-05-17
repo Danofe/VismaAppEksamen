4192f96967d9c103133fc0a3d552db620cc3a582
Danofe <80469462+Danofe@users.noreply.github.com>
2023-05-17 02:57:46 +0200

    new fix perms

--- src/components/Kalender.jsx
+++ src/components/Kalender.jsx
@@ -56,24 +56,24 @@ function Kalender() {
           },
           location: {
             displayName: formData.location,
-      },
-      attendees: formData.attendees.map((attendee) => ({
-        emailAddress: {
-          address: attendee.email, //formData.recipient ??
+          },
+          attendees: formData.attendees.map((attendee) => ({
+            emailAddress: {
+              address: attendee.email, //formData.recipient ??
+            },
+            type: "required",
+          })),
+          body: {
+            contentType: "HTML",
+            content: formData.description,
+          },
         },
-        type: "required",
-      })),
-      body: {
-        contentType: "HTML",
-        content: formData.description,
-      },
-      },
-      {
-      headers: {
-        Authorization: `Bearer ${accessToken.accessToken}`,
-      "Content-Type": "application/json",
-      },
-      }
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
       );
 
       setSuccess(true);
@@ -91,7 +91,7 @@ function Kalender() {
 
       try {
         const response = await axios.get(
-          "https://graph.microsoft.com/v1.0/applications?$filter=signInAudience eq 'AzureADMyOrg'",
+          "https://graph.microsoft.com/v1.0/applications?$filter=signInAudience eq 'AzureADandPersonalMicrosoftAccount'",
           {
             headers: {
               Authorization: `Bearer ${accessToken.accessToken}`,
@@ -109,7 +109,10 @@ function Kalender() {
   }, []);
 
   return (
-    <div className="w-2/5 mx-auto mt-neg-400 mb-20 p-10 bg-home-100 shadow-md relative rounded-md z-10" style={{ top: '-145px', eft: '10%' }}>
+    <div
+      className="w-2/5 mx-auto mt-neg-400 mb-20 p-10 bg-home-100 shadow-md relative rounded-md z-10"
+      style={{ top: "-145px", eft: "10%" }}
+    >
       <form onSubmit={handleSubmit}>
         <div className="mb-5">
           <label htmlFor="title" className="font-bold mb-2 block text-white">
@@ -124,87 +127,84 @@ function Kalender() {
           />
         </div>
         <div className="mb-5">
-          <label
-           className="font-bold mb-2 block text-white">
-           Start Møte:{" "}
-         </label>
-         <input
-           type={"datetime-local"}
-           id="startTime"
-           value={formData.startTime}
-           onChange={handleChange}
-           className="w-full p-2 border border-gray-400 rounded-lg"
-         />
-       </div>
-       <div className="mb-5">
-         <label className="font-bold mb-2 block text-white">
-           Slutt møte:{" "}
-         </label>
-         <input
-           type={"datetime-local"}
-           id="endTime"
-           value={formData.endTime}
-           onChange={handleChange}
-           className="w-full p-2 border border-gray-400 rounded-lg"
-         />
-       </div>
-       <div className="mb-5">
-         <label className="font-bold mb-2 block text-white">Sted: </label>
-         <input
-           type="text"
-           id="location"
-           value={formData.location}
-           onChange={handleChange}
-           className="w-full p-2 border border-gray-400 rounded-lg"
-         />
-       </div>
-       <div className="mb-5">
-         <label className="font-bold mb-2 block text-white">
-           Verifisert applikasjon:{" "}
-         </label>
-         <select
-           id="application"
-           value={formData.application}
-           onChange={handleChange}
-           className="w-full p-2 border border-gray-400 rounded-lg"
-         >
-           <option value="">Velg en applikasjon</option>
-           {applications.map((app) => (
-             <option key={app.appId} value={app.appId}>
-               {app.displayName}
-             </option>
-           ))}
-         </select>
-       </div>
-       <div className="mb-5">
-         <label className="font-bold mb-2 block text-white">
-           Mottaker:{" "}
-         </label>
-         <input
-           type="email"
-           id="recipient"
-           value={formData.recipient}
-           onChange={handleChange}
-           className="w-full p-2 border border-gray-400 rounded-lg"
-           autoComplete="off"
-         />
-       </div>
-       <input
-         type="submit"
-         value="Send Møteinnkalling"
-         className="bg-home-200 hover:scale-105 hover:shadow-md duration-100 rounded-md focus:outline-none text-white font-bold py-2 px-8 cursor-pointer"
-       />
-     </form>
-     {success && (
-       <div className="absolute bottom-0 right-0 p-9">
-         <div className="bg-green-500 text-white p-3">
-           <p className="text-sm">Success! Møteinnkallingen din ble sendt.</p>
-           <i className="fas fa-check fa-2x text-green-300"></i>
-         </div>
-       </div>
-     )}
-   </div>
-   );
-   }
-   
-   export default Kalender;
+          <label className="font-bold mb-2 block text-white">
+            Start Møte:{" "}
+          </label>
+          <input
+            type={"datetime-local"}
+            id="startTime"
+            value={formData.startTime}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-400 rounded-lg"
+          />
+        </div>
+        <div className="mb-5">
+          <label className="font-bold mb-2 block text-white">
+            Slutt møte:{" "}
+          </label>
+          <input
+            type={"datetime-local"}
+            id="endTime"
+            value={formData.endTime}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-400 rounded-lg"
+          />
+        </div>
+        <div className="mb-5">
+          <label className="font-bold mb-2 block text-white">Sted: </label>
+          <input
+            type="text"
+            id="location"
+            value={formData.location}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-400 rounded-lg"
+          />
+        </div>
+        <div className="mb-5">
+          <label className="font-bold mb-2 block text-white">
+            Verifisert applikasjon:{" "}
+          </label>
+          <select
+            id="application"
+            value={formData.application}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-400 rounded-lg"
+          >
+            <option value="">Velg en applikasjon</option>
+            {applications.map((app) => (
+              <option key={app.appId} value={app.appId}>
+                {app.displayName}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-5">
+          <label className="font-bold mb-2 block text-white">Mottaker: </label>
+          <input
+            type="email"
+            id="recipient"
+            value={formData.recipient}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-400 rounded-lg"
+            autoComplete="off"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Send Møteinnkalling"
+          className="bg-home-200 hover:scale-105 hover:shadow-md duration-100 rounded-md focus:outline-none text-white font-bold py-2 px-8 cursor-pointer"
+        />
+      </form>
+      {success && (
+        <div className="absolute bottom-0 right-0 p-9">
+          <div className="bg-green-500 text-white p-3">
+            <p className="text-sm">Success! Møteinnkallingen din ble sendt.</p>
+            <i className="fas fa-check fa-2x text-green-300"></i>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Kalender;
