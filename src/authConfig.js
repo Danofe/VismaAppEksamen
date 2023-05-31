87a6643f60c3a537cdd45087a2f254e1b4f9dcce
Danofe <80469462+Danofe@users.noreply.github.com>
2023-05-31 04:07:59 +0200

    MER

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { useAuthContext } from "./context/authContext";
 
 //Hook for å få tak i authentication config dataen fra context.
