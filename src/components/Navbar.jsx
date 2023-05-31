56138178b16b8624d31a9046b23d12870df864c2
Danofe <80469462+Danofe@users.noreply.github.com>
2023-05-31 06:26:02 +0200

    plassering fjerning av module packege i server

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,7 @@ function Navbar() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
       />
-
       <div className="flex items-center flex-shrink-0 text-white mr-2 absolute hover:scale-110 duration-300"></div>
-
       <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[250px] overflow-y-auto text-center bg-gray-600">
         <Link to="/">
           <div className="flex-shrink-0 text-white ml-3 p-6 mr-7 hover:scale-110 duration-300">
@@ -34,7 +32,6 @@ function Navbar() {
         <Link to="/Kalender">
           <div className="p-2.5 mt-5 flex items-center hover:scale-105 rounded-md px-4 duration-300 cursor-pointer hover:bg-rose-600 text-white">
             <i className="bi-calendar2-date-fill" />
-
             <span className="text-[15px] ml-4 text-gray-200 font-bold">
               Kalender
             </span>
